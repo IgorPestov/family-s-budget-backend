966cbb4dabf5f09a38326acbd45dff28013404a5
IgorPestov <ipestov.exceedteam@gmail.com>
2020-11-23 10:53:21 +0300

    added validate token

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -1,8 +1,8 @@
-
 const jwt = require("jsonwebtoken");
-const authHelper = require("../helper/authHelper")
-const tokenModel = require("../models/tokenSchema")
-const decode = require("jwt-decode")
+const authHelper = require("../helper/authHelper");
+const tokenModel = require("../models/tokenSchema");
+const decode = require("jwt-decode");
+const { json } = require("body-parser");
 require("dotenv").config();
 
 module.exports = async (req, res, next) => {
@@ -11,21 +11,26 @@ module.exports = async (req, res, next) => {
     res.status(401).json({ message: "Token not provided!" });
     return;
   }
-  const token = authHeader.replace("Bearer ", "");
-  const tokenId = decode(token).id
-  const tokenCheck = await tokenModel.findOne({tokenId})
-
- const test = await authHelper.updateTokens(tokenCheck.userId)
-  try {   
-   const payload = jwt.verify(test.accessToken, process.env.secret);
-   if(payload.type !== 'access') {
-       res.status(401).json({message: "Invalid token 1"})
-       return;
-   }
+  let test;
+  try {
+    const token = authHeader.replace("Bearer ", "");
+    const tokenId = decode(token).id;
+    const tokenCheck = await tokenModel.findOne({ tokenId });
+    test = await authHelper.updateTokens(tokenCheck.userId);
   } catch (e) {
-      if(e instanceof jwt.TokenExpiredError) {
-          res.status(401).json({message:'Token expired!'})
-      }
+    res.status(401).json({ message: "Неверный токен" });
+  }
+
+  try {
+    const payload = jwt.verify(test.accessToken, process.env.secret);
+    if (payload.type !== "access") {
+      res.status(401).json({ message: "Invalid token 1" });
+      return;
+    }
+  } catch (e) {
+    if (e instanceof jwt.TokenExpiredError) {
+      res.status(401).json({ message: "Token expired!" });
+    }
     if (e instanceof jwt.JsonWebTokenError) {
       res.status(401).json({ message: "Invalid token!" });
       return;
