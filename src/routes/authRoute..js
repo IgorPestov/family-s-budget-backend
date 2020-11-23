8f12438496f72300e59aea234484c51b316b0812
IgorPestov <ipestov.exceedteam@gmail.com>
2020-11-23 17:26:04 +0300

    fixed link

--- src/routes/authRoute..js
+++ src/routes/authRoute..js
@@ -5,6 +5,6 @@ const authControllers = require("../controllers/auth");
 auth.post("/login", authControllers.login);
 auth.post("/signup", authControllers.signup);
 auth.post("/check-mail", authControllers.checkEmailForResetPasword);
-auth.post("/reser-password", authControllers.resetPassword);
+auth.post("/reset-password", authControllers.resetPassword);
 
 module.exports = auth;
